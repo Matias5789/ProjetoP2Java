266033f63407c042364116c6e50b943082b5b124
Matias5789 <mateusrjm4@gmail.com>
2024-10-14 19:55:08 -0300

    criacao da base do projeto

---
+++ emprestimo-livros/src/main/java/com/biblioteca/emprestimo_livros/EmprestimoLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.emprestimo_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoLivrosApplication.class, args);
+	}
+
+}
---
+++ emprestimo-livros/src/test/java/com/biblioteca/emprestimo_livros/EmprestimoLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.emprestimo_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
